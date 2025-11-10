2000316853b36d149bd9a59176b0aefaf0bc8663
kotulc <claytulak@gmail.com>
2025-11-09 21:13:32 -0700

    Fix test_model cache

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -6,19 +6,19 @@ import spacy
 from app.core.utils import models
 
 
-@pytest.mark.parametrize("getter, args, expected_type", [
-    (models.get_acceptability, ["test"], list),
-    (models.get_classifier, ["test"], dict),
-    (models.get_embedding, ["test"], numpy.ndarray),
-    (models.get_generator, ["test"], str),
-    (models.get_keybert, ["test"], list),
-    (models.get_polarity, ["test"], dict),
-    (models.get_sentiment, ["test"], dict),
-    (models.get_spam, ["test"], list),
-    (models.get_toxicity, ["test"], list),
-    (models.get_yake, ["test"], list),
+@pytest.mark.parametrize("getter, expected_type", [
+    (models.get_acceptability, list),
+    (models.get_classifier, dict),
+    (models.get_embedding, numpy.ndarray),
+    (models.get_generator, str),
+    (models.get_keybert, list),
+    (models.get_polarity, dict),
+    (models.get_sentiment, dict),
+    (models.get_spam, list),
+    (models.get_toxicity, list),
+    (models.get_yake, list),
 ])
-def test_models(monkeypatch, getter, args, expected_type):
+def test_models(monkeypatch, getter, expected_type):
     # Save original debug setting
     original_debug = models.settings.debug
 
@@ -32,11 +32,32 @@ def test_models(monkeypatch, getter, args, expected_type):
 
     # Test with debug False (real)
     monkeypatch.setattr(models.settings, "debug", False)
+    getter.cache_clear()
     real_func = getter()
-    assert callable(mock_func)
+    assert callable(real_func)
 
     real_result = real_func("test")
     assert isinstance(real_result, expected_type)
+
+    # Restore original debug setting
+    monkeypatch.setattr(models.settings, "debug", original_debug)
+
+
+def test_spacy(monkeypatch):
+    # Save original debug setting
+    original_debug = models.settings.debug
+
+    # Test with debug True (mock)
+    monkeypatch.setattr(models.settings, "debug", True)
+    mock_func = models.get_spacy()
+    mock_result = mock_func("test")
+
+    # Test with debug False (real)
+    monkeypatch.setattr(models.settings, "debug", False)
+    models.get_spacy.cache_clear()
+    real_func = models.get_spacy()
+    real_result = real_func("test")
+    assert type(mock_result) == type(real_result)
 
     # Restore original debug setting
     monkeypatch.setattr(models.settings, "debug", original_debug)
